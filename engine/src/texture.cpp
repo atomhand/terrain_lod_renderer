3855275be757eef73431e68e3894edb61e66658b
atomhand <thessalikon@live.co.uk>
2025-12-01 21:27:13 +0000

    Fix-could only import 3 channel textures

--- engine/src/texture.cpp
+++ engine/src/texture.cpp
@@ -25,7 +25,30 @@ Engine::Texture Engine::Texture::Import(const char* filePath) {
     unsigned char *data = stbi_load(path.string().c_str(), &width, &height, &nrChannels, 0);
 
     if(data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        GLenum format;
+        switch (nrChannels)
+        {
+        case 1:
+            format = GL_RED;
+            break;
+        case 2:
+            format = GL_RG;
+            break;
+        case 3:
+            format = GL_RGB;
+            break;
+        case 4:
+            format = GL_RGBA;
+            break;
+        default:
+            std::cout << "Warning : loading texture with supported numbers of channels " << nrChannels << ") " << filePath << std::endl;
+            break;
+        }
+
+
+        // NOTE - currently, regardless of the input format, I'm just storing it as a 3 channel texture
+        // This is clearly something that could be improved on in the future
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     } else {
         std::cout << "Failed to load texture at path " << path.string() << std::endl;
