5642f2a2beec2341221d3f1cac5c5d92985af2f8
atomhand <thessalikon@live.co.uk>
2026-04-26 03:49:20 +0100

    Default config change

--- engine/include/world.h
+++ engine/include/world.h
@@ -57,7 +57,7 @@ namespace Engine {
         int shadowTestingMode = 0;
         bool previewCascades = false;
         int numCascades = 5;
-        float lightPssmFactor = 0.15f;
+        float lightPssmFactor = 0.f;
 
         // Terrain render     
         bool stochasticBlending = true;
