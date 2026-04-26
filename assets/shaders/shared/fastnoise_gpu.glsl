d5682c1517e645c343dc864dc76d5c293df98539
atomhand <thessalikon@live.co.uk>
2026-04-26 03:53:03 +0100

    fastnoise license

--- assets/shaders/shared/fastnoise_gpu.glsl
+++ assets/shaders/shared/fastnoise_gpu.glsl
@@ -1,6 +1,30 @@
 #ifndef FASTNOISE_GPU_GLSL
 #define FASTNOISE_GPU_GLSL
 
+/*
+MIT License
+
+Copyright (c) 2020 Jordan Peck
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #define PRIMES_X int(0xF797C5C7)
 #define PRIMES_Y int(0x6C060C89)
 #define PRIMES_Z int(0x465FD04F)
